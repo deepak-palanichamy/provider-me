10ab3000ca53d993de560a521eb8ae6ddb2df053
Deepak Palanisamy <deepakpalanichamy@gmail.com>
2023-03-10 08:42:12 +0530

    implment oauth2 authoorization_code flow using keycloak

---
+++ src/main/java/dev/datapirate/providerme/ProviderMeApplication.java
@@ -0,0 +1,13 @@
+package dev.datapirate.providerme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProviderMeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderMeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/datapirate/providerme/ProviderMeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.datapirate.providerme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProviderMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
